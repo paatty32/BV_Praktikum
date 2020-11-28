40d48adba6fdcb04a51c5609f51daac6c7747938
paatty32 <patrick.boadu.boafo@hotmail.de>
2020-11-28 20:32:40 +0100

    Logik in eine Methode ausgelagert.

--- BV_Praktikum/BV_Praktikum1/Quelle.cpp
+++ BV_Praktikum/BV_Praktikum1/Quelle.cpp
@@ -7,19 +7,21 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char * argv[]) {
+int SIZE = 512;
 
-	const int SIZE = 512;
+void readImg(String &pfad, String &datei) {
+    ifstream dicomDatei(pfad + datei);
 
-	//Datei einlesen
-	ifstream dicomDatei(argv[1]);
+    //Groesse der Datei herausfinden
+    dicomDatei.seekg(0, dicomDatei.end); //Setzt den zeiger ausgehend von der ersten Zeile aufs Ende der Datei
+    int dateiGroesse = dicomDatei.tellg();
+    dicomDatei.seekg(0, dicomDatei.beg);
 
-	//Groesse der Datei herausfinden
-	dicomDatei.seekg(0, dicomDatei.end); //Setzt den zeiger ausgehend von der ersten Zeile aufs Ende der Datei
-	int dateiGroesse = dicomDatei.tellg();
-	dicomDatei.seekg(0, dicomDatei.beg);
+    //Ende des Header herausfinden
+    if(dateiGroesse - ( (SIZE * SIZE) * 2) < 0){
+        SIZE = 256;
+    }
 
-	//Ende des Header herausfinden
     int headEnde = dateiGroesse - ( (SIZE * SIZE) * 2);
 
     cout << "HeadEnde: " << headEnde << endl;
@@ -30,38 +32,50 @@ int main(int argc, char * argv[]) {
     dicomDatei.seekg(headEnde, dicomDatei.beg); //Setze den Zeiger ab dem HeadEnde
 
     //Buffer mit inhalt der Bilddateien befüllen
-	dicomDatei.read((char*) buffer, dateiGroesse - headEnde); //Fuegt die anzahl, die im 2. Parameter angegeben ist in das array was im ersten Parameter angegeben ist
+    dicomDatei.read((char*) buffer, dateiGroesse - headEnde); //Fuegt die anzahl, die im 2. Parameter angegeben ist in das array was im ersten Parameter angegeben ist
 
-	dicomDatei.close();
+    dicomDatei.close();
 
-	Mat bild(SIZE, SIZE,CV_16U);
-	//16bit Pixel 
-	int pixel;
+    Mat bild(SIZE, SIZE,CV_16U);
+    //16bit Pixel
+    int pixel;
 
-	//Mat Objekt befuellen
-	for(int i = 0; i < SIZE; i++){
-	    for(int j = 0; j < SIZE; j++){
-	        pixel;
-	        bild.at<ushort>(i,j) = buffer[(i * SIZE) + j]; //Pixel
-	    }
-	}
-    imshow("Normales Bild", bild);
+    //Mat Objekt befuellen
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            pixel;
+            bild.at<ushort>(i,j) = buffer[(i * SIZE) + j]; //Pixel
+        }
+    }
+    imshow(pfad + datei, bild);
 
-	double min, max;
-	minMaxLoc(bild, &min, &max);
-	cout << "kleinster Grauwert: " << min << " Größter GRauwert: " << max << endl;
+    double min, max;
+    minMaxLoc(bild, &min, &max);
+    cout << "kleinster Grauwert: " << min << " Größter GRauwert: " << max << endl;
 
-	Mat scal;
+    Mat scal;
     convertScaleAbs(bild, scal, 255.0 / (max - min), -min * (255.0 / (max - min)));
 
-	imshow("Dicom", scal);
-	waitKey();
-	
-	//Content printen:
-    //cout.write(buffer, dateiGroesse);
-	
+    imshow(datei, scal);
 
 
-	delete[] buffer;
-	return 0;
+    //Content printen:
+//cout.write(buffer, dateiGroesse);
+
+    delete[] buffer;
+}
+
+int main(int argc, char * argv[]) {
+
+	//Datei einlesen
+	String pfad = argv[1];
+	String datei = "0.dcm";
+    String datei1 = "1.dcm";
+    String datei2 = "2.dcm";
+
+    readImg(pfad, datei);
+    readImg(pfad, datei1);
+    readImg(pfad, datei2);
+    waitKey();
+    return 0;
 }
